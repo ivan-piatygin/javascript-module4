64683544016249e2043970fbd22ce9205df007a7
Ivan4489 <ivan.piatygin@hotmail.com>
2023-01-14 13:51:58 +0200

    auto-6

--- js/atuo-6.js
+++ js/atuo-6.js
@@ -1,11 +1,11 @@
 function filterArray(numbers, value) {
   const filteredNumbers = [];
 
-  for (let i=0; i<numbers.length; i+=1) {
-    if (numbers[i] > value) {
-      filteredNumbers.push(numbers[i]);
+  numbers.forEach(function (number, index) {
+    if (number > value) {
+      filteredNumbers.push(number);
     }
-  }
+  })
 
   return filteredNumbers;
 }
