96a4356e1c49946ca0149ee8241a49311fa0b19b
Ivan4489 <ivan.piatygin@hotmail.com>
2023-01-19 20:37:08 +0200

    auto-47

--- js/auto-37.js
+++ js/auto-37.js
@@ -1,26 +1,101 @@
 // Change code below this line
-const getFriends = users => {
-    users.reduce((allFriends, user) => {
-    allFriends.push(...user.friends);
-  }, []);
+// const getFriends = users => {
+//     users.reduce((allFriends, user) => {
+//     allFriends.push(...user.friends);
+//   }, []);
 
- return allFriends;
-}
+//  return allFriends;
+// }
 
-// const friends = getFriends(user);
+// // const friends = getFriends(user);
 
-const getFriendStats = (acc, friend) => {
- if (!acc.hasOwnProperty(friend)) {
-   acc[friend] = 0;
- }
- acc[friend] += 1;
+// const getFriendStats = (acc, friend) => {
+//  if (!acc.hasOwnProperty(friend)) {
+//    acc[friend] = 0;
+//  }
+//  acc[friend] += 1;
 
- return acc;
-}
+//  return acc;
+// }
 
-const getTotalFriendCount = users => users.reduce(getFriendStats, {});
+// const getTotalFriendCount = users => users.reduce(getFriendStats, {});
 
 // const friendCount = getTotalFriendCount(friends);
 // console.log(friendCount)
 
 // Change code above this line
+
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+
+const getTotalFriendCount = users => {
+   users.reduce( (acc, user) => {
+    acc.push(...user.friends);
+    return acc.length;
+   }, [])
+};
+
+console.log(getTotalFriendCount(users));
---
+++ js/try.js
@@ -0,0 +1,5 @@
+function classicAdd(a, b, c) {
+    return a + b + c;
+  }
+
+console.log(classicAdd(1,1,1));
